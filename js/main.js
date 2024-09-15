366d5d234c036fe8d8a09d227422d8001f8db872
Mohamed Rabee <166071170+Mohamerabee98@users.noreply.github.com>
2024-09-15 10:41:39 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function () {
+
+
+    $(".lds-ellipsis").fadeOut(1000, function () {
+        $('.looding').slideUp(1000)
+
+        $("body").css("overflow", "visible")
+    })
+})
+
+
+
+//Aos animation js
+AOS.init();
+
+
+// change bg nav bar on scroll
+function changebg() {
+    var navbar = document.getElementById('nav-bg');
+    var scrollValue = window.scrollY;
+    if (scrollValue < 10) {
+        navbar.classList.remove('bg-color')
+    }
+    else {
+        navbar.classList.add('bg-color')
+    }
+}
+
+window.addEventListener('scroll', changebg)
+
+
+let overlay = document.querySelector('#overlay')
+let AboutBtn = document.querySelector('#AboutBtn')
+let iconClose = document.querySelector('#iconClose')
+let body = document.querySelector('body')
+
+AboutBtn.addEventListener('click', function () {
+    overlay.classList.replace('d-none', 'd-flex')
+    $("body").css("overflow", "hidden")
+
+
+})
+
+iconClose.addEventListener('click', function () {
+    overlay.classList.replace('d-flex', 'd-none')
+    $("body").css("overflow", "visible")
+})
+
+
+// Slide Work
+var swiperWork = new Swiper(".swiper-work", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+let globalid = document.querySelector('#globalid');
+console.log(globalid);
